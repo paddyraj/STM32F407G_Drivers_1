f507ee6fcea13cb11a27971be4a15e182c3c3fc4
Raj Padgaonkar <paddyraj18@gmail.com>
2024-11-19 04:27:06 -0500

    Commit-Drivers-3

--- Drivers/inc/STM32F407_gpio_driver.h
+++ Drivers/inc/STM32F407_gpio_driver.h
@@ -10,11 +10,11 @@
 #include "STM32F4xx.h"
 #include <Stdint.h>
 typedef struct {
-	uint8_t GPIOPinNumber; 				/* Pin Number */
-	uint8_t GPIOmode;					/*GPIO Mode*/
-	uint8_t GPIOSpeed;					/*GPIO Speed*/
-	uint8_t GPIOOutputType;				/*GPIO Output Type*/
-	uint8_t GPIO_pull_Up_Dwn;			/*GPIO pull-up or pull-down */
+	uint8_t GPIOPinNumber; 				/*GPIO Pin Number check ref. @Pin_Number*/
+	uint8_t GPIOmode;					/*GPIO Mode check ref. @PIN_MODES */
+	uint8_t GPIOSpeed;					/*GPIO Speed check ref.@SPEED */
+	uint8_t GPIOOutputType;				/*GPIO Output Type check ref. @OUTPUT_TYPE */
+	uint8_t GPIO_pull_Up_Dwn;			/*GPIO pull-up or pull-down check ref. @PULL_UP_DOWN_*/
 	uint8_t Alt_Fn_Mode;				/*GPIO alternate function mode*/
 
 }GPIO_PinConfig_t;
@@ -26,6 +26,60 @@ typedef struct {
 	GPIO_PinConfig_t GPIO_PinConfig;	/*This holds the GPIO pin Configuration Settings*/
 
 	}GPIO_Handle_t ;
+
+/*
+ * @PIN_MODES
+ * GPIO possible modes
+ * */
+#define GPIO_MODE_IN 			0 		//INPUT MODE
+#define GPIO_MODE_OUT 			1		//GENRAL PURPOSE OUTPUT MODE
+#define GPIO_MODE_ALT_FN 		2		//ALTERNATE FUNCTION MODE
+#define GPIO_MODE_ANG_MD 		3		//ANALOG MODE
+
+#define GPIO_MODE_IRQ_FT 		4		//Interrupt falling-egde trigger
+#define GPIO_MODE_IRQ_RT		5		//Interrupt rising-egde trigger
+#define GPIO_MODE_IRQ_RFT		6		//Interrupt rising and falling-edge trigger
+
+
+
+//@OUTPUT_TYPE
+
+#define GPIO_MODE_PP			0		//Output type is push-pull
+#define GPIO_MODE_OD			1		//output type is open drain
+
+
+// @SPEED
+
+#define GPIO_MODE_L_SPEED		0		//Low speed
+#define GPIO_MODE_M_SPEED		1		//Medium speed
+#define GPIO_MODE_F_SPEED		2		//Fast speed
+#define GPIO_MODE_H_SPEED		3		//High Speed
+
+//@PULL_UP_DOWN
+
+#define GPIO_MODE_NOPP			0		//NO pull-up or pull-down
+#define GPIO_MODE_PU			1		//Pull-up Configuration
+#define GPIO_MODE_PD			3		//Pull-down Configuration
+
+
+//@Pin_Number
+
+#define GPIO_PIN_0				0		//GPIOx pin number 0
+#define GPIO_PIN_1				1		//GPIOx pin number 1
+#define GPIO_PIN_2				2		//GPIOx pin number 2
+#define GPIO_PIN_3				3		//GPIOx pin number 3
+#define GPIO_PIN_4				4		//GPIOx pin number 4
+#define GPIO_PIN_5				5		//GPIOx pin number 5
+#define GPIO_PIN_6				6		//GPIOx pin number 6
+#define GPIO_PIN_7				7		//GPIOx pin number 7
+#define GPIO_PIN_8				8		//GPIOx pin number 8
+#define GPIO_PIN_9				9		//GPIOx pin number 9
+#define GPIO_PIN_10				10		//GPIOx pin number 10
+#define GPIO_PIN_11				11		//GPIOx pin number 11
+#define GPIO_PIN_12				12		//GPIOx pin number 12
+#define GPIO_PIN_13				13		//GPIOx pin number 13
+#define GPIO_PIN_14				14		//GPIOx pin number 14
+#define GPIO_PIN_15				15		//GPIOx pin number 15
 
 /**********************************************************************************************************
  *		API supported by this driver
--- Drivers/src/STM32F407_gpio_driver.c
+++ Drivers/src/STM32F407_gpio_driver.c
@@ -32,63 +32,132 @@
 	 * ****************************************************************************************/
 
 	void GPIO_PClk_Ctr(GPIO_RegDef_t *pGPIOx , uint8_t EnorDi){
+		if (EnorDi == ENABLE){
+			if(GPIOx == GPIOA){
+			GPIOA_PCLK_EN();
+			}
+			else if(GPIOx == GPIOB)
+			{
+					GPIOB_PCLK_EN();
+			}
+			else if(GPIOx == GPIOB)
+			{
+					GPIOB_PCLK_EN();
+			}
+			else if(GPIOx == GPIOC)
+			{
+				 	GPIOC_PCLK_EN();
+			}
+			else if(GPIOx == GPIOD)
+			{
+					GPIOD_PCLK_EN();
+			}
+			else if (GPIOx == GPIOE)
+			{
+					GPIOE_PCLK_EN();
+			}
+			else if (GPIOX == GPIOF){
+					GPIOE_PCLK_EN();
+			}
+			else if (GPIOX == GPIOF){
+					GPIOF_PCLK_EN();
+			}
+			else if (GPIOX == GPIOG){
+					GPIOG_PCLK_EN();
+			}
+			else if (GPIOX == GPIOH){
+					GPIOH_PCLK_EN();
+			}
+			else if (GPIOX == GPIOI){
+			}
+		}
 
+		else {
 
-	}
+			if(GPIOx == GPIOA){
+			GPIOA_PCLK_DI()();
+			}
+			else if(GPIOx == GPIOB)
+			{
+					GPIOB_PCLK_DI()();
+			}
+			else if(GPIOx == GPIOB)
+			{
+					GPIOB_PCLK_DI()();
+			}
+			else if(GPIOx == GPIOC)
+			{
+				 	GPIOC_PCLK_DI()();
+			}
+			else if(GPIOx == GPIOD)
+			{
+					GPIOD_PCLK_DI()();
+			}
+			else if (GPIOx == GPIOE)
+			{
+					GPIOE_PCLK_DI()();
+			}
+			else if (GPIOX == GPIOF){
+					GPIOE_PCLK_DI()();
+			}
+			else if (GPIOX == GPIOF){
+					GPIOF_PCLK_DI()();
+			}
+			else if (GPIOX == GPIOG){
+					GPIOG_PCLK_DI()();
+			}
+			else if (GPIOX == GPIOH){
+					GPIOH_PCLK_DI()();
+			}
+			else if (GPIOX == GPIOI){
+			}
+			}
+		}
 
 	/*
 	 * Initialize , De-Initialize
 	 * */
 
 	/***************************************************************************************
-	 * @fn 					- GPIO_PClk_Ctr
+	 * @fn 					- GPIO_Init
 	 *
-	 * @brief 				-
+	 * @brief 				- GPIO port initiation function
 	 *
+	 * @param[in] 			- GPIO_Handle_t - Handler Function to which include base address of GPIO and
+	 *						  GPIO configuration type.
 	 *
+	 * @param[in]			- none
 	 *
-	 * @param[in]			-
+	 * @param[in]			- none
 	 *
-	 * @param[in]			-
+	 * @return				- none
 	 *
-	 * @param[in]			-
-	 *
-	 *
-	 * @return				-
-	 *
-	 *
-	 * @note				-
-	 *
-	 *
-	 *
+	 * @note				- none
 	 *
 	 * ****************************************************************************************/
 	void GPIO_Init(GPIO_Handle_t *pGPIOHandle){
-
-
+		//1. configure the mode of GPIO
+		//2. configure the speed
+		//3. configure the pull-up or pull-down register
+		//4. configure the output type
+		//5. configure the alt-function type
 	}
 
 	/***************************************************************************************
-	 * @fn 					- GPIO_PClk_Ctr
+	 * @fn 					- GPIO_DeInit
 	 *
-	 * @brief 				-
+	 * @brief 				- GPIO  de-initiatation function
 	 *
+	 * @param[in]			- GPIO base Address of the given GPIO port
 	 *
+	 * @param[in]			- none
 	 *
-	 * @param[in]			-
-	 *
-	 * @param[in]			-
-	 *
-	 * @param[in]			-
+	 * @param[in]			- none
 	 *
 	 *
-	 * @return				-
+	 * @return				- none
 	 *
-	 *
-	 * @note				-
-	 *
-	 *
-	 *
+	 * @note				- none
 	 *
 	 * ****************************************************************************************/
 	void GPIO_DeInit(GPIO_RegDef_t *pGPIOx){
@@ -99,16 +168,52 @@
 	/*
 	 * Data Read and Write
 	 * */
+	/***************************************************************************************
+		 * @fn 					- GPIO_Read_From_Input_Pin
+		 *
+		 * @brief 				- GPIO read data from Input pin specified
+		 *
+		 * @param[in]			- GPIO_RegDef_t - GPIO base Address of the given GPIO port
+		 *
+		 * @param[in]			- PinNumber - Unsigned integer in 1 Byte for pin number
+		 *
+		 * @param[in]			- none
+		 *
+		 * @return				- unsigned integer of 1 byte.
+		 *
+		 * @note				- none
+		 *
+		 * ****************************************************************************************/
 
 	uint8_t GPIO_Read_From_Input_Pin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber){
 
 
 	}
+
+	/***************************************************************************************
+			 * @fn 					- GPIO_Read_From_Input_Port
+			 *
+			 * @brief 				- GPIO read data from Input port specified
+			 *
+			 * @param[in]			- GPIO_RegDef_t - GPIO base Address of the given GPIO port
+			 *
+			 * @param[in]			- none
+			 *
+			 * @param[in]			- none
+			 *
+			 * @return				- unsigned integer of 2 byte
+			 *
+			 * @note				- none
+			 *
+			 * ****************************************************************************************/
+
 	uint16_t GPIO_Read_From_Input_Port(GPIO_RegDef_t *pGPIOx){
 
 
 
 	}
+
+
 	void GPIO_To_Output_Port(GPIO_RegDef_t *pGPIOx , uint8_t Value){
 
 	}
